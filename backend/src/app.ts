bf7e6553fe863c94660df4a53f1caea487f4705d
depermana12 <deddiapermana97@gmail.com>
2024-10-15 20:53:31 +0700

    enable all cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import userRoute from "./routes/user";
 import noteRoute from "./routes/note";
@@ -9,6 +10,7 @@ import authorization from "./middlewares/authzHandler";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
